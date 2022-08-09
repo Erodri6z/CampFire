f4ff2e823ec686e1e0d9266870cec785d5b1e78e
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-09 12:23:02 -0500

    add a nav bar

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+class Story:
+  def __init__(self, plot, genre, themes):
+    self.plot = plot
+    self.genre = genre
+    self.themes = themes
+
+stories = [
+  Story('People are lost but then they are like even more lost', 'Comedy', 'Psychological Horor')
+]
+
+
 
 def home(request):
   return HttpResponse('<h1>Hello this is the first page, like a page named home or something</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def stories_index(request):
+  return render(request, 'stories/index.html', { 'stories': stories })
