eae2fcc8321d64ce1fa6d4c2f4cc025026836a2d
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-09 20:52:27 -0500

    add a view function

--- main_app/views.py
+++ main_app/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 class Story:
-  def __init__(self, plot, genre, themes):
+  def __init__(self, plot, genre, theme):
     self.plot = plot
     self.genre = genre
-    self.themes = themes
+    self.theme = theme
 
 stories = [
-  Story('People are lost but then they are like even more lost', 'Comedy', 'Psychological Horor')
+  Story('People are lost but then they are like even more lost', 'Comedy', 'Psychological Horor'),
+  Story('People are lost but then they like stay lost bc they suck at travel.', 'Comedy', 'Psychological Horor'),
 ]
 
 
-
 def home(request):
-  return HttpResponse('<h1>Hello this is the first page, like a page named home or something</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
