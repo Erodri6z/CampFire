f4d35d5c9cbde2a3717caa0acdb84f5899ef3866
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-10 21:20:09 -0500

    impliment sign up function

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Story
 # Create your views here.
 
@@ -29,6 +30,10 @@ class StoryCreate(CreateView):
   fields = '__all__'
   successful_url = '/stories/'
 
+  def form_valid(self, form):
+    form.instance.user = self.request.user  
+    return super().form_valid(form)
+
 class StoryUpdate(UpdateView):
   model = Story
   fields = '__all__'
@@ -36,3 +41,17 @@ class StoryUpdate(UpdateView):
 class StoryDelete(DeleteView):
   model = Story
   success_url = '/stories/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('stories_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
